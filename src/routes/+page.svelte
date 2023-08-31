a38dd0356ee54fed5bd030109d9ccea16edef7f6
Kyubey <kyubey@Kyubey.lan>
2023-08-30 22:12:02 -0700

    media queries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,8 @@
 	let styles = {
 		'shadow': '0 0 1px black',
 		'many-shadows': '0 0 1px black,',
-		'cool-filter' : 'drop-shadow(-1px -1px 0 red) drop-shadow(1px 1px 0 blue)'
+		'cool-filter' : 'drop-shadow(-1px -1px 0 red) drop-shadow(1px 1px 0 blue)',
+		'invert-filter' : 'drop-shadow(-1px -1px 0 cyan) drop-shadow(1px 1px 0 yellow)'
 	};
 
 	styles['many-shadows'] = styles['many-shadows'].repeat(4).slice(0, -1);
@@ -70,22 +71,33 @@
 	}
 
 	section {
+		/* tall screens ONLY */
+		max-height: 100vh;
+
+		/* square and wide screens */
+
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 		height: 100rem;
+		min-height: 100vh;
 		position: relative;
 
 		/* filter: var(--cool-filter); */
 	}
 	section::before {
+	/* dots */
 		content: '';
 		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100vw;
 		height: 100rem;
+		min-height: 100vh;
+
+		
+
 		background: url('/images/dots.png');
 		background-size: cover;
 		image-rendering: pixelated;
@@ -97,6 +109,7 @@
 		font-size: 9vw;
 		position: absolute;
 		width: 10rem;
+		/* height: 60vh; */
 		bottom: 1rem;
 		right: 1rem;
 		width: max-content;
@@ -107,7 +120,10 @@
 		padding: 1rem;
 	}
 	.name {
-		font-size: 10rem;
+		/* tall styles */
+		font-size: 11vw;
+
+		/* square and wide styles */
 		width: max-content;
 		word-wrap: none;
 		white-space: nowrap;
@@ -117,6 +133,7 @@
 		transform: rotate(-50deg) translate(-80%, 100%);
 		transition: all 100ms ease-out;
 		overflow: hidden;
+		background: black;
 	}
 	.name2 {
 		transform: rotate(-50deg) translate(20%, 100%);
@@ -127,7 +144,7 @@
 	}
 	.name:hover {
 		/* z-index: 2; */
-		filter: var(--cool-filter);
+		/* filter: var(--cool-filter); */
 	}
 	h1 {
 		font-weight: bold;
@@ -140,8 +157,9 @@
 		/* font-weight: 100; */
 		color: white;
 		text-rendering: optimizeLegibility;
-		background-color: #000;
+		/* background-color: #000; */
 		image-rendering: pixelated;
+		filter: var(--cool-filter);
 
 	}
 	.japanese {
@@ -155,14 +173,69 @@
 		writing-mode: vertical-rl;
 	}
 	
+	/* write a media query that applys styles to desktop screens */
+	@media (min-width: 1024px) {
+		:global(body) {
+			max-height: initial;
+			height: 200vh;
+			overflow-y: auto !important;
+		}
+		section {
+			height: 200vh;
+			max-height: 200vh;
+			/* position: relative; */
+		}
+		section::before {
+			height: 200vh;
+		}
+		.about {
+			height: auto !important;
+			/* position: absolute; */
+		}
+	}
+
+
+		/* make an alternate version of the site with a white background */
+		/* trust me it looks suoer cool */
+	@media(prefers-color-scheme: light) {
+		:global(body) {
+			background-color: white !important;
+			filter: invert(1);
+		}
+		h1, p {
+			background: none !important;
+			/* text-shadow: 0 0 5px black; */
+			/* color: black; */
+			/* filter: var(--invert-filter); */
+		}
+		.about {
+			/* border: 1px solid black; */
+			/* background: none; */
+			/* this is for when i figure out how to make the bubbles black */
+			/* background: white; */
+		}
+		.name {
+
+			/* this is for when i figure out how to make the bubbles black */
+			/* background: black; */
+		}
+		section::before {
+			/* filter: invert(1); */
+			/* filter: var(--invert-filter); */
+		}
+	}
 
 	:global(body) {
-		/* background: url("/images/dots.png"); */
 		background-color: black;
 		background-size: cover;
 		image-rendering: pixelated;
 		image-rendering: crisp-edges;
 		background-repeat: no-repeat;
 		overflow-x: hidden;
+		/* cool case where overflow-y actually effects */
+		/* (its the name that overflowing on the btm)  */
+		overflow-y: hidden;
+		/* tall screens ONLY */
+		max-height: 100vh;
 	}
 </style>
